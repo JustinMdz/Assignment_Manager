b760d5a4cb7a20fd4351b9e67eb088d900830c99
JustinMdz <121051670+JustinMdz@users.noreply.github.com>
2024-09-25 14:23:58 -0600

    [ADD] Add Spring Boot Structure

---
+++ src/main/java/org/una/programmingIII/Assignment_Manager/AssignmentManagerApplication.java
@@ -0,0 +1,13 @@
+package org.una.programmingIII.Assignment_Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/una/programmingIII/Assignment_Manager/AssignmentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.programmingIII.Assignment_Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
