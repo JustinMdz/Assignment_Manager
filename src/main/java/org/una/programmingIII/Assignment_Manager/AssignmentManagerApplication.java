caecd3135d851f51f58c7a6c3b2b6f449c78715a
JustinMdz <121051670+JustinMdz@users.noreply.github.com>
2024-10-08 17:48:00 -0600

    [ADD] Add Swagger Tool

--- src/main/java/org/una/programmingIII/Assignment_Manager/AssignmentManagerApplication.java
+++ src/main/java/org/una/programmingIII/Assignment_Manager/AssignmentManagerApplication.java
@@ -1,7 +1,11 @@
 package org.una.programmingIII.Assignment_Manager;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AssignmentManagerApplication {
@@ -9,5 +13,4 @@ public class AssignmentManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AssignmentManagerApplication.class, args);
 	}
-
 }
